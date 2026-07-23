d6c21c7ce224c4a6d6fbb4cd59929e40c90e7f82
Harshit_CH@2528 <143932415+Harshit-Choubey@users.noreply.github.com>
2026-07-24 00:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (57.93%) - LeetSync

--- LeetCode/733-flood-fill/flood-fill.cpp
+++ LeetCode/733-flood-fill/flood-fill.cpp
@@ -16,30 +16,26 @@ public:
         int m = image.size();
         int n = image[0].size();
         
-        // Queue to store the coordinates of the pixels to process
         queue<pair<int, int>> q;
         
-        // Start with the initial pixel
         q.push({sr, sc});
-        image[sr][sc] = color; // Change color immediately when adding to queue
+        image[sr][sc] = color;         
         
-        // Directions for Up, Down, Left, Right
         vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
-        // Process the queue
         while (!q.empty()) {
             auto [r, c] = q.front();
             q.pop();
             
-            // Check all 4 neighboring pixels
+
             for (auto [dr, dc] : dirs) {
                 int nr = r + dr;
                 int nc = c + dc;
                 
-                // If neighbor is within bounds AND has the original color
+
                 if (nr >= 0 && nr < m && nc >= 0 && nc < n && image[nr][nc] == original_color) {
-                    image[nr][nc] = color; // Paint it
-                    q.push({nr, nc});      // Add to queue to check its neighbors later
+                    image[nr][nc] = color; 
+                    q.push({nr, nc});  
                 }
             }
         }
