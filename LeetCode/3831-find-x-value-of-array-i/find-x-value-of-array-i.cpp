df8ef53fc41d9599ef9d2800ee6fd4d867298185
Harshit_CH@2528 <143932415+Harshit-Choubey@users.noreply.github.com>
2026-09-22 01:43:48 +0530

    Time: 180 ms (24.11%) | Memory: 151.2 MB (55.36%) - LeetSync

---
+++ LeetCode/3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public: 
+    vector<long long >resultArray(vector<int> & nums, int k){
+        vector<long long > ans(k, 0);
+        vector<long long > dp(k,0);
+        
+        for(int x : nums){
+            vector<long long > ndp(k, 0);
+            
+            ndp[x % k]++;
+            for(int r = 0; r<k; r++){
+                int nr = (r * (x % k)) % k;
+                ndp[nr] += dp[r];
+        }
+        dp  = ndp;
+        for(int r =0; r < k; r++) 
+        ans[r] += dp[r];
+    }
+    return ans;
+    }
+};
